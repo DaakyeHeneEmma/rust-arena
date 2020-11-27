a285f1cb9ef04f41aabbe9bc3ea3ff3bf78c60d7
DaakyeHeneEmma <emmaatta.me@yahoo.com>
2020-11-27 01:46:53 +0000

    initializer

---
+++ project/target/debug/deps/project.d
@@ -0,0 +1,5 @@
+C:\Users\USER\Desktop\myNextProject\rust-arena\project\target\debug\deps\project.exe: src\main.rs
+
+C:\Users\USER\Desktop\myNextProject\rust-arena\project\target\debug\deps\project.d: src\main.rs
+
+src\main.rs:
---
+++ project/target/debug/project.d
@@ -0,0 +1,1 @@
+C:\Users\USER\Desktop\myNextProject\rust-arena\project\target\debug\project.exe: C:\Users\USER\Desktop\myNextProject\rust-arena\project\src\main.rs
