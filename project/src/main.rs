3d4f1b25946839de9de30d0d07708528bde3511f
DaakyeHeneEmma <49214219+DaakyeHeneEmma@users.noreply.github.com>
2021-08-14 02:42:09 +0000

    changes

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+
+#[derive(Clone,Debug)]
+
+struct Set(Box<i32>, Box<i32>);
+
+#[derive(Debug,Clone)]
+struct Car {
+    model: String,
+    name: String
+}
+
+ macro_rules! cars {
+     ($car:ident) => {
+         fn $car(){
+            println!( "carz {}", stringify!($car));
+         }
+     };
+ } 
+
+//  pub fn person(a::i32)-> Result<i32>{
+
+//  }
+
+ cars!(year);
+ cars!(month);
+ cars!(day);
+
+fn main(){
+    year();
+    month();
+    day();
+
+    // let cordinates = Set(Box::new(1), Box::new(2));
+    // println!("a set of numbers {:?} ",cordinates);
+
+    // let new_car = Car{model: String::from("toyota"), name: String::from("corolla")};
+    // println!("the car is {:?} ", new_car);
+    // println!("the car is {} ", new_car.name);
+    
 }
